c18653d66d471ae197400bfc7a9cb5b95562bc79
sunny-prakash <sunny.pprakash@gmail.com>
2021-05-08 18:46:01 +0530

    Update App.js
    added ol li

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol key={"relativeList"}>
+                 <li key={"relativeListItem1"}>Mohan</li>
+                 <li key={"relativeListItem2"}>Sohan</li>
+                 <li key={"relativeListItem3"}>Rohan</li>
+                 <li key={"relativeListItem4"}>Tinu</li>
+               </ol>
             </div>
         )
     }
